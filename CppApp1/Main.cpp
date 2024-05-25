7460c0fe567cb90449086815a6feb66b0ec4f77b
Arslan Ahmad <ars.ahm6@gmail.com>
2024-05-25 17:07:11 +0500

    First example.

--- CppApp1/Main.cpp
+++ CppApp1/Main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
+#include "mylib.h"
 using namespace std;
+using namespace myns;
 
+int number = 30;
+
+namespace yourns {
+	int number = 20;
+}
+
+using namespace yourns;
 
 int main() 
 {
-	cout << "Enter a number: ";
-	int n;
-	cin >> n;
-	cout << "You entered " << n << endl;
+	int n1, n2;	// declaration
+
+	cout << "Enter first number: ";
+	cin >> n1;
+
+	cout << "Enter second number: ";
+	cin >> n2;
+
+	/*cin >> n1 >> n2;*/
+
+	int sum = n1 + n2; // definition
+
+	cout << "The sum of " << n1 << " and " << n2 << " is " << sum;
+
+	/*cout << "The sum of ";
+	cout << n1;
+	cout << " and ";
+	cout << n2;
+	cout << " is ";
+	cout << sum;*/
+
+	system("pause");
+	return 0;
 }
---
+++ CppApp1/mylib.h
@@ -0,0 +1,11 @@
+#pragma once
+
+int globalNumber = 100;
+
+namespace myns {
+	int number = 10;
+}
+
+namespace ns2 {
+	int number2 = 200;
+}
